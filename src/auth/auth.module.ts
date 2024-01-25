ef02138cad1770a3875449b89e2e06e28dacc699
jordi.nodejs <jordi.nodejs@gmail.com>
2024-01-25 16:19:04 +0100

    Remove unused import in auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,7 @@
-import { UsersService } from './../users/users.service';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
-import { JwtModule, JwtService } from '@nestjs/jwt';
+import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from '../constants';
 import { LocalStrategy } from './strategies/local-strategy';
 import { AuthService } from './auth.service';
